8728af2e713f69c223783da31ba6847c784e9eca
unknown <sparshkumar14998@gmail.com>
2021-05-29 13:41:45 +0530

    event binding completed

--- src/app/event-binding/event-binding.component.ts
+++ src/app/event-binding/event-binding.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventBindingComponent implements OnInit {
 
+  public greeting = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  HandleClick (event: any) { // without defining the event to be of type any, this will throw an error, so remind it.
+    //console.log (event);
+    if (this.greeting) {
+      this.greeting = ''
+    } else {
+      this.greeting = 'the button has been clicked';
+    }
   }
 
 }
